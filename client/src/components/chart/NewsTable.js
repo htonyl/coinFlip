d96be2f45451f144f531ba01130f1b6a0d368373
Chou Yi-Hung <hank0982@gmail.com>
2018-05-10 23:12:29 +0800

    modified news table

--- client/src/components/chart/NewsTable.js
+++ client/src/components/chart/NewsTable.js
@@ -10,6 +10,7 @@ class NewsTable extends Component {
         this.state = {
           news: [],
           posts: [],
+          active: true,
           topk: [],
           topkIdx: 0
         };
@@ -19,21 +20,25 @@ class NewsTable extends Component {
         this.firebase.database().ref('scoredNews').once('value', (snap) => {
           let val = snap.val();
           let news = Object.keys(val).map(elem => val[elem]);
-          that.setState({ news: news }, ()=>that.sortTopk());
+          that.setState({ news: news }, ()=>that.sortTopk(true));
         });
         this.firebase.database().ref('scoredPosts').once('value', (snap) => {
           let val = snap.val();
           let posts = Object.keys(val).map(elem => val[elem]);
-          that.setState({ posts: posts }, ()=>that.sortTopk());
+          that.setState({ posts: posts }, ()=>that.sortTopk(true));
           console.log(posts);
         });
+        this.handleClick = this.handleClick.bind(this)
     }
 
-    sortTopk(){
+    sortTopk(pos){
         const k = 5;
         let topk = this.state.posts.concat(this.state.news);
-        topk.sort((a,b) => b.score-a.score);
-
+        if(pos){
+          topk.sort((a,b) => b.score-a.score);  
+        }else{
+          topk.sort((a,b) => a.score-b.score);
+        }
         // Split sorted news & posts into batches of size k
         let topkSplit = [];
         for(var i = 1; i*k < topk.length; i++){
@@ -46,21 +51,34 @@ class NewsTable extends Component {
         });
     }
 
+    handleClick(){
+      this.setState({ active: !this.state.active})
+      if(this.state.active){
+        this.sortTopk(false)
+      }else{
+        this.sortTopk(true)
+      }
+    }
+
     render() {
+      const { active } = this.state
       let topk = this.state.topk.length ? this.state.topk[this.state.topkIdx] : [];
       return (
-          <table style={{width:'80%'}}>
+          <table style={{width:'100%', paddingTop: "24px"}}>
             <thead>
               <tr>
-                <th style={{width:'80%', padding:'10px 20px', backgroundColor:'#F9F9F9', textAlign:'left', fontSize: 24}}>
+                <th style={{width:'100%', padding:'10px 20px', backgroundColor:'#F9F9F9', textAlign:'left', fontSize: 24}}>
                   Top News / Posts
+                  <Button color='red' toggle active={active} onClick={this.handleClick} size='tiny' floated='right'>
+                    Toggle
+                  </Button>
                 </th>
-                <th></th>
+                <th> </th>
               </tr>
             </thead>
             <tbody>
               <tr>
-                <th style={{width:'80%', padding:'0 20px 10px 20px', margin:'0 10px', backgroundColor:'#F9F9F9'}}>
+                <th style={{width:'100%', padding:'0 20px 10px 20px', margin:'0 10px', backgroundColor:'#F9F9F9'}}>
                   <List>
                     {
                       topk.map((elem)=>{
@@ -78,9 +96,7 @@ class NewsTable extends Component {
                     }
                   </List>
                 </th>
-                <th>
-                    <Prediction {...this.props} firebase={this.firebase}></Prediction>
-                </th>
+                
               </tr>
             </tbody>
           </table>
--- client/src/components/chart/NewsTableRow.js
+++ client/src/components/chart/NewsTableRow.js
@@ -37,26 +37,28 @@ class NewsTableRow extends Component {
       let poc = ((item.score-1)*100).toPrecision(4);
       let type = item.source.search('reddit') < 0 ? 'newspaper' : 'reddit';
       return (
-        <Button icon style={rowStyle}>
-          <div style={{ width: '5%', minWidth: 10, display: 'inline-block'}}>
-            <Icon name={type}></Icon>
-          </div>
-          <div style={titleStyle}>
-            {item.title}
-          </div>
-          <div style={authorStyle}>
-            by <span style={{fontStyle: 'italic'}}>{item.author}</span>
-          </div>
-          <div style={scoreStyle}>
-            <span style={{color: item.score >= 1 ? '#1e983a' : '#db2828'}}>
-              {item.score >= 1 ?
-                <Icon name='caret up' /> :
-                <Icon name='caret down' />
-              }
-              {poc}
-            </span>
-          </div>
-        </Button>
+        <a href= {item.url} target="_blank">
+          <Button icon style={rowStyle}>
+            <div style={{ width: '5%', minWidth: 10, display: 'inline-block'}}>
+              <Icon name={type}></Icon>
+            </div>
+            <div style={titleStyle}>
+              {item.title}
+            </div>
+            <div style={authorStyle}>
+              by <span style={{fontStyle: 'italic'}}>{item.author}</span>
+            </div>
+            <div style={scoreStyle}>
+              <span style={{color: item.score >= 1 ? '#1e983a' : '#db2828'}}>
+                {item.score >= 1 ?
+                  <Icon name='caret up' /> :
+                  <Icon name='caret down' />
+                }
+                {poc + '%'}
+              </span>
+            </div>
+          </Button>
+        </a>
       );
     }
 }
